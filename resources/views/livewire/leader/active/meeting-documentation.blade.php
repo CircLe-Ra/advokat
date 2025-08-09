dc98090117eb25dc029f3f5cd87bc87c870524c3
CircLe-Ra <herrylapandera@gmail.com>
2025-08-09 09:58:39 +0900

    up

--- resources/views/livewire/leader/active/meeting-documentation.blade.php
+++ resources/views/livewire/leader/active/meeting-documentation.blade.php
@@ -37,16 +37,16 @@ new class extends Component {
 <x-partials.sidebar :id-detail="$this->meeting->legalCase?->id" menu="leader-active-case" :back="route('leader.active.case.page', ['case' => $this->meeting->legal_case_id, 'status' => 'schedule'], absolute: false)"
                     active="Penanganan Kasus / Jadwal Pertemuan / {{ $this->meeting->legalCase?->title }} / Hasil Pertemuan">
     <x-slot:information>
-        @if($this->case->status !== 'rejected')
+        @if($this->meeting->legalCase?->status !== 'rejected')
             <div class="flex flex-col  dark:border-zinc-700 dark:bg-zinc-800 flex-shrink-0">
                 <div
                     class="flex flex-col items-center border border-zinc-100 bg-zinc-100 shadow-md dark:border-zinc-700 dark:bg-zinc-900 w-full py-6 px-4 rounded-lg">
                     <flux:heading size="xl" class="text-center text-xl mb-2">PENGACARA</flux:heading>
                     <div class="h-20 w-20 rounded-full border border-zinc-200 dark:border-zinc-700 overflow-hidden">
-                        <flux:avatar size="xl" class="size-full " :name="$this->case->lawyer?->user->name" :initials="$this->case->lawyer?->user->initials()"/>
+                        <flux:avatar size="xl" class="size-full " :name="$this->meeting->legalCase?->lawyer?->user->name" :initials="$this->meeting->legalCase?->lawyer?->user->initials()"/>
                     </div>
-                    <div class="text-sm font-semibold mt-2">{{ $this->case->lawyer?->user->name }}</div>
-                    <div class="text-xs text-gray-500">{{ $this->case->lawyer?->user->email }}</div>
+                    <div class="text-sm font-semibold mt-2">{{ $this->meeting->legalCase?->lawyer?->user->name }}</div>
+                    <div class="text-xs text-gray-500">{{ $this->meeting->legalCase?->lawyer?->user->email }}</div>
                 </div>
             </div>
         @endif
